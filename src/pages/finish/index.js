4fe62f8299c325304d59cd9a51411e80915c0361
SMLukwiya <sundaymorganl@gmail.com>
2020-09-21 08:33:10 +0300

    add sample animation

--- src/pages/finish/index.js
+++ src/pages/finish/index.js
@@ -1,36 +1,44 @@
 import React from 'react';
 import { Row, Col, Checkbox } from 'antd';
 import {Link} from 'react-router-dom';
+import {CSSTransition} from 'react-transition-group';
 
-import classes from './finish.module.css';
+import  classes from './finish.module.css';
 import Header from '../../components/Header';
 import Menu from '../../components/Menu';
 import BackButton from '../../components/common/BackButton';
 import Button from '../../components/common/Button';
 
 const Confirm = (props) => (
-  <div className={classes.Container}>
-    <Header color='#C1839F' />
-    <Row className={classes.Row}>
-      <Col span={4}>
-        <Menu />
-      </Col>
-      <Col span={2} className={classes.BackButton}>
-        <BackButton history={props.history} />
-      </Col>
-      <Col span={18} className={classes.ConfirmCol}>
-        <div className={classes.Title}>finish</div>
-        <div className={classes.TextContainer}>
-          <Checkbox className={classes.Text}>& I accept the Terms of Use and Privacy Policy</Checkbox>
-        </div>
-        <div className={classes.ButtonContainer}>
-          <Link to="/post/howitworks/create/confirm/payment">
-            <Button title='all done' small color='#C1839F' />
-          </Link>
-        </div>
-      </Col>
-    </Row>
-  </div>
+  <CSSTransition
+    in={true}
+    timeout={5000}
+    classNames={'finishPage-'}
+    unmountOnExit={true}
+    appear={true}>
+    <div className={classes.Container}>
+      <Header color='#C1839F' />
+      <Row className={classes.Row}>
+        <Col span={4}>
+          <Menu />
+        </Col>
+        <Col span={2} className={classes.BackButton}>
+          <BackButton history={props.history} />
+        </Col>
+        <Col span={18} className={classes.ConfirmCol}>
+          <div className={classes.Title}>finish</div>
+          <div className={classes.TextContainer}>
+            <Checkbox className={classes.Text}>& I accept the Terms of Use and Privacy Policy</Checkbox>
+          </div>
+          <div className={classes.ButtonContainer}>
+            <Link to="/post/howitworks/create/confirm/payment">
+              <Button title='all done' small color='#C1839F' />
+            </Link>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  </CSSTransition>
 )
 
 export default Confirm;
